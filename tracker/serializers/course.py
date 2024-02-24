67e59aee5d532e8cdd1820921053044a4ba6c6f3
julia <juliakofmana@gmail.com>
2024-02-24 12:22:21 +0300

    homework 24.2 edit

--- tracker/serializers/course.py
+++ tracker/serializers/course.py
@@ -1,14 +1,18 @@
 from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
 
-from tracker.models import Course
+from tracker.models import Course, Lesson
+from tracker.serializers.lesson import LessonSerializer
 
 
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+    # lessons = serializers.SlugRelatedField(slug_field='name', many=True, queryset=Lesson.objects.all())
 
     def get_lesson_count(self, obj):
-        return obj.lesson_set.count()
+        return obj.lessons.count()
 
     class Meta:
         model = Course
-        fields = ['id', 'name', 'description', 'lesson_count']
+        fields = ['id', 'name', 'description', 'lesson_count', 'lessons']
